0c766c0b5ebd474b5dac42a06cbe518617decb01
Rangon Roy Utsab <rangonroy@outlook.com>
2026-05-15 15:58:07 +0600

    feat: add product data types

---
+++ src/types/product.ts
@@ -0,0 +1,15 @@
+export type ProductCategory = {
+  id: number;
+  name: string;
+  image: string;
+};
+
+export type Product = {
+  id: number;
+  title: string;
+  slug: string;
+  price: number;
+  description: string;
+  category: ProductCategory;
+  images: string[];
+};
